322cae8f1415da400d3a996b45a9e9ba04a756cb
Pon-Taichi <saramanda164@gmail.com>
2022-12-26 23:55:06 +0900

    ToDo追加時にテーブルに反映されるよう修正、editをInputに配置

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,11 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { STATUS } from 'src/model/status.model';
@@ -44,5 +50,17 @@ export class TodoListComponent implements AfterViewInit {
     this.todoService.getTodos().subscribe((todos) => {
       this.dataSource.data = todos;
     });
+  }
+
+  addTodo(todo: TodoViewModel): void {
+    const todos = this.dataSource.data;
+    todos.push(todo);
+    this.dataSource.data = todos;
+  }
+
+  save(todo: TodoViewModel): void {
+    if (todo.isEdit) {
+      todo.isEdit = !todo.isEdit;
+    }
   }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,9 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { STATUS } from 'src/model/status.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ViewMode, VIEW_MODE } from 'src/model/view-mode.model';
+
 import { TodoViewModel } from 'src/viewModel/todo.viewModel';
 import { TodoService } from '../service/todo.service';
+import { TodoListComponent } from '../todo-list/todo-list.component';
 
 @Component({
   selector: 'app-todo',
@@ -12,19 +11,31 @@ import { TodoService } from '../service/todo.service';
   styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  viewMode: ViewMode = VIEW_MODE.LIST;
+
+  @ViewChild(TodoListComponent) private todoListComponent!: TodoListComponent;
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {}
 
-  addTodo(taskName: string) {
+  addTodo(taskName: string): void {
     const todo: TodoViewModel = {
-      id: '',
+      id: '4',
       taskName: taskName,
       status: 'New',
       deadline: new Date(),
       isEdit: false,
     };
 
-    this.todoService.addTodo(todo).subscribe();
+    this.todoService.addTodo(todo).subscribe((todo) => {
+      this.todoListComponent.addTodo(todo);
+    });
+  }
+
+  showTodoList(): void {
+    this.viewMode = VIEW_MODE.LIST;
+  }
+  showCompletedTasks(): void {
+    this.viewMode = VIEW_MODE.COMPLETED;
   }
 }
